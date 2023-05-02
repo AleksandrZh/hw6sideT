193915584e36dafe22910907785f6825f626449c
ZhmakinAA <ZhmakinAA@hotmail.ru>
2023-05-02 13:18:11 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace hw6sideT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a, b, c;
+
+            Console.WriteLine("Сторона a:");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Сторона b:");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Сторона c:");
+            c = double.Parse(Console.ReadLine());
+
+
+            Triangle trian = new Triangle(a, b, c);
+
+            trian.Show();
+
+
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,59 @@
+﻿namespace hw6sideT
+{
+    internal class Triangle
+    {
+        private double sideA;
+        private double sideB;
+        private double sideC;
+        private double perimeter;
+        private double area;
+
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+
+
+        }
+
+
+        private void CheckExistenceTriangle()
+        {
+            if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
+            {
+                perimeter = Perimeter();
+                area = Area();
+                Console.WriteLine($"Треугольник со сторонами  A: {sideA} B: {sideB} C: {sideC} существует. Его периметер : {perimeter}, площадь: {area}");
+            }
+            else
+            {
+                Console.WriteLine($"Треугольник со сторонами A: {sideA} B: {sideB} C: {sideC} не существует.");
+            }
+        }
+
+        private double Perimeter()
+        {
+            return sideA + sideB + sideC;
+        }
+
+        private double Area()
+        {
+
+            double p = ((sideA + sideB + sideC) / 2);
+
+            return Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+        }
+
+        public void Show()
+        {
+
+            CheckExistenceTriangle();
+
+        }
+
+
+    }
+
+}
